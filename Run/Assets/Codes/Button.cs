b8877196f7a529c9a4a2b82659145f80553f3b71
goksel6 <32982444+goksel6@users.noreply.github.com>
2018-04-05 18:37:41 +0300

    v2

---
+++ Run/Assets/Codes/Button.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour {
+    Char_Code c= new Char_Code();
+    public GameObject obj;
+    public  void ButtonOnClick()
+    {
+        obj.SetActive(false);
+        c.ButtonRestart();
+    }
+}
---
+++ Run/Assets/Codes/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    Rigidbody2D enemyBody;
+    public float enemySpeed;
+    bool isGround;
+    Transform groundCheck;
+    const float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+    public bool moveRight;
+
+	// Use this for initialization
+	void Start () {
+        enemyBody = GetComponent<Rigidbody2D>();
+        groundCheck = transform.Find("GroundCheck");
+	}
+	
+	void Update () {
+     
+    }
+    private void FixedUpdate()
+    {
+
+        isGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        enemyBody.velocity = (moveRight) ? new Vector2(-enemySpeed, 0) : new Vector2(enemySpeed, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D theobject)
+    {
+        if (theobject.gameObject.tag == "Trap")
+        {         
+            transform.localScale = (moveRight) ? new Vector2(-1, 1) : new Vector2(1, 1);
+            moveRight = !moveRight;
+        }
+    }
+}
